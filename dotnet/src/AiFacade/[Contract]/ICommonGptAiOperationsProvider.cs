a06abb920110b3d2c095d3edd367216c9a871808
KornSW <email@tobiaskorn.de>
2025-12-01 13:06:03 +0100

    initial commit (moved from PoC-Repo)

---
+++ dotnet/src/AiFacade/[Contract]/ICommonGptAiOperationsProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AI.KornSW {
+
+  public interface ICommonGptAiOperationsProvider {
+
+    string CallWebSearchApi(string prompt, object inputData = null);
+    T CallWebSearchApi<T>(string prompt, object inputData = null) where T : class;
+
+    byte[] CallImageEditApi(byte[] inputImageBytes, string prompt, byte[] maskImageBytes = null);
+    byte[] CallImageGeneratorApi(string prompt);
+
+  }
+
+}
---
+++ dotnet/test/AiFacade.Tests/OpenAiProviderTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AI.KornSW {
+
+  [TestClass()]
+  public class OpenAiProviderTests {
+
+    [TestMethod()]
+    public void OpenAiProviderTest1() {
+
+     //Assert.AreEqual(863154666, inferredId);
+
+    }
+
+  }
+
+}
